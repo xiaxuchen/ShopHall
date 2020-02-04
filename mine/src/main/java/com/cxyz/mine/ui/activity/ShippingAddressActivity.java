db997265bbf10767dcc829a7174d29adee01b014
lixing <599940031@qq.com>
2020-02-04 10:11:51 +0800

    完成 颜色更改
    Signed-off-by: lixing <599940031@qq.com>

--- mine/src/main/java/com/cxyz/mine/ui/activity/ShippingAddressActivity.java
+++ mine/src/main/java/com/cxyz/mine/ui/activity/ShippingAddressActivity.java
@@ -18,22 +18,10 @@ import java.util.List;
 @Route(path = "/mine/ShippingAddressActivity",group = "mine")
 public class ShippingAddressActivity extends BaseActivity<ShippingAddressPresenter> implements IShippingAddressView {
     private RecyclerView recyclerView;
-   // private List<Adress> adressList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.mine_activity_shipping_address);
-        //initList（）方法用于给playerList填充数据
-        //initList();
-
-        //recyclerView = findViewById(R.id.addressRecycler);
-        //创建LinearLayoutManager，用于决定RecyclerView的布局方式
-        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ShippingAddressActivity.this);
-        //recyclerView.setLayoutManager(linearLayoutManager);
-        //创建适配器
-        //AddressAdapter adapter = new AddressAdapter(adressList);
-        //recyclerView.setAdapter(adapter);
         iPresenter.fetch();
     }
 
@@ -69,11 +57,4 @@ public class ShippingAddressActivity extends BaseActivity<ShippingAddressPresent
         AddressAdapter adapter = new AddressAdapter(list);
         recyclerView.setAdapter(adapter);
     }
-
-
-   /* private void initList() {
-            //通过调用构造方法，赋值
-            Adress lisan = new Adress("李三", "18879792833", "江西.萍乡", "安源区安源大道110");
-            adressList.add(lisan);
-    }*/
 }
