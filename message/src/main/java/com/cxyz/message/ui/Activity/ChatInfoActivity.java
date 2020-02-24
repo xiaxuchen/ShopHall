e240c54233468ef968ac7a7b4851ce50e5f7a81a
Super丶执念 <956822950@qq.com>
2020-02-24 15:52:09 +0800

    2.24修改界面

--- message/src/main/java/com/cxyz/message/ui/Activity/ChatInfoActivity.java
+++ message/src/main/java/com/cxyz/message/ui/Activity/ChatInfoActivity.java
@@ -16,6 +16,7 @@ import com.cxyz.mvp.activity.BaseActivity;
 import com.cxyz.mvp.ipresenter.IBasePresenter;
 import com.cxyz.utils.ColorsUtil;
 import com.cxyz.utils.ToastUtil;
+import com.cxyz.widget.HeaderBar;
 import com.qmuiteam.qmui.widget.QMUIRadiusImageView;
 
 import java.util.ArrayList;
@@ -29,8 +30,7 @@ public class ChatInfoActivity extends BaseActivity implements RecyclerViewAdapte
     private List<String> dataTitle;     //标题（谁的消息）
     private List<String> datasContent;  //内容（消息内容）
     private List<String> datasTime;     //时间（消息时间）
-    private QMUIRadiusImageView ivLogisticsInfo;
-    private QMUIRadiusImageView  ivDiscontInfo;
+    private HeaderBar hbChatinfotitle;
     @Override
     protected Object getContentView() {
         return R.layout.message_activity_chatinfo;
@@ -40,6 +40,7 @@ public class ChatInfoActivity extends BaseActivity implements RecyclerViewAdapte
     public void initView() {
         recycler = (RecyclerView)findViewById(R.id.recycler);
         recycler.setLayoutManager(new LinearLayoutManager(this));
+        hbChatinfotitle=findViewById(R.id.hbChatinfotitle);
         datas();//插入数据
         //更新界面
         updateInterface();
@@ -53,6 +54,12 @@ public class ChatInfoActivity extends BaseActivity implements RecyclerViewAdapte
 
     @Override
     public void setEvent() {
+        hbChatinfotitle.setBackClickListener(new HeaderBar.OnBackClickListener() {
+            @Override
+            public void onBackClick(View v) {
+                getActivity().finish();
+            }
+        });
 
     }
 
