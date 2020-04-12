55d8fd46e0d0a7d7c6af9a976220cd6b0762d45d
L15579986114 <2697845116@qq.com>
2020-04-12 11:52:24 +0800

    修改修改

--- shopcar/src/main/java/com/cxyz/car/ui/activity/StoreDetailActivity.java
+++ shopcar/src/main/java/com/cxyz/car/ui/activity/StoreDetailActivity.java
@@ -39,6 +39,7 @@ public class StoreDetailActivity extends BaseActivity<StoreDetailPresenter> impl
     private StoreDetail.StoreDetailBean storeDetailBean;//店铺详细信息
 
     private ImageView qmuiIvStoreDetailStoreImageSmall;//店铺小图片
+    private ImageView ivStoreDetailFocus;//添加关注按钮
 
     private LinearLayout llGetService;//练习客服
     private LinearLayout llStoreComment;//店铺评论
@@ -80,7 +81,7 @@ public class StoreDetailActivity extends BaseActivity<StoreDetailPresenter> impl
         qmuiIvStoreDetailStoreImageSmall=findViewById(R.id.qmuiIvStoreDetailStoreImageSmall);
         llGetService=findViewById(R.id.llgetService);
         llStoreComment=findViewById(R.id.llStoreComment);
-
+        ivStoreDetailFocus=findViewById(R.id.ivStoreDetailFocus);
         updateListener=new UpdateListener() {
             @Override
             public User OnUpdate(User oldUser, User newUser) {
