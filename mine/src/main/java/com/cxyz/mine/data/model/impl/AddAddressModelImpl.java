3207fbffc93dd05e4fb9de04c7f9864daffacaf8
lixing <599940031@qq.com>
2020-05-01 19:21:53 +0800

    修改搜索界面
    Signed-off-by: lixing <599940031@qq.com>

--- mine/src/main/java/com/cxyz/mine/data/model/impl/AddAddressModelImpl.java
+++ mine/src/main/java/com/cxyz/mine/data/model/impl/AddAddressModelImpl.java
@@ -12,6 +12,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class AddAddressModelImpl extends IAddAddressModel {
+
     private CheckResult<Boolean> checkResult;
     private boolean flag;
     @Override
