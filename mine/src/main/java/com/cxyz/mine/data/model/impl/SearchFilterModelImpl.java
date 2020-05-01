7f6990e464a4a60237c60cb4714bd656d72f2381
lixing <599940031@qq.com>
2020-05-01 19:47:17 +0800

    筛选功能
    Signed-off-by: lixing <599940031@qq.com>

---
+++ mine/src/main/java/com/cxyz/mine/data/model/impl/SearchFilterModelImpl.java
@@ -0,0 +1,76 @@
+package com.cxyz.mine.data.model.impl;
+
+import android.accounts.NetworkErrorException;
+
+import com.cxyz.http.CommonOkHttpClient;
+import com.cxyz.http.listener.DisposeDataHandler;
+import com.cxyz.http.listener.DisposeDataListener;
+import com.cxyz.http.request.RequestParams;
+import com.cxyz.http.response.CheckResult;
+import com.cxyz.mine.data.model.ISearchFilterModel;
+import com.cxyz.mine.ui.adapter.entity.Filter;
+import com.cxyz.utils.GsonUtil;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONException;
+
+import java.util.List;
+
+public class SearchFilterModelImpl extends ISearchFilterModel {
+    @Override
+    public void loadData(OnGetModel onGetModel) {
+        try {
+            RequestParams params = new RequestParams();
+            CommonOkHttpClient.get("http://rest.apizza.net/mock/230ac06df6f24f16acec6dcbbc686092/search/filter",params,
+                    new DisposeDataHandler(new DisposeDataListener() {
+                        @Override
+                        public void onSuccess(Object responseObj) {
+                            try {
+                                CheckResult<List<Filter>> checkResult = (CheckResult<List<Filter>>) GsonUtil.fromJson(responseObj.toString(),
+                                        new TypeToken<CheckResult<List<Filter>>>(){}.getType());
+                                onGetModel.getModel(checkResult.getData());
+                                System.out.println(checkResult.getData());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Object error) {
+
+                        }
+                    }));
+        } catch (NetworkErrorException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void loadMarketData(OnGetMarketModel onGetMarketModel) {
+        try {
+            RequestParams params = new RequestParams();
+            CommonOkHttpClient.get("http://rest.apizza.net/mock/230ac06df6f24f16acec6dcbbc686092/search/filter",params,
+                    new DisposeDataHandler(new DisposeDataListener() {
+                        @Override
+                        public void onSuccess(Object responseObj) {
+                            CheckResult<List<Filter>> checkResult = null;
+                            try {
+                                checkResult = (CheckResult<List<Filter>>) GsonUtil.fromJson(responseObj.toString(),
+                                        new TypeToken<CheckResult<List<Filter>>>(){}.getType());
+                                onGetMarketModel.getAdModel(checkResult.getData());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Object error) {
+
+                        }
+                    }));
+        } catch (NetworkErrorException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ mine/src/main/java/com/cxyz/mine/data/model/impl/SearchResultShopModelImpl.java
@@ -0,0 +1,54 @@
+package com.cxyz.mine.data.model.impl;
+
+import android.accounts.NetworkErrorException;
+
+import com.cxyz.http.CommonOkHttpClient;
+import com.cxyz.http.listener.DisposeDataHandler;
+import com.cxyz.http.listener.DisposeDataListener;
+import com.cxyz.http.request.RequestParams;
+import com.cxyz.http.response.CheckResult;
+import com.cxyz.mine.data.model.ISearchResultShopModel;
+import com.cxyz.mine.ui.adapter.entity.SearchShops;
+import com.cxyz.utils.GsonUtil;
+import com.google.gson.reflect.TypeToken;
+import com.just.agentweb.LogUtils;
+
+import org.json.JSONException;
+
+import java.util.List;
+
+public class SearchResultShopModelImpl extends ISearchResultShopModel {
+    private CheckResult<List<SearchShops>> checkResult;
+    @Override
+    public void loadData(OnGetModel onGetModel) {
+        /**
+         *   添加用户的收货地址，响应是否添加成功
+        */
+        try {
+            RequestParams params = new RequestParams();
+            CommonOkHttpClient.put("http://rest.apizza.net/mock/230ac06df6f24f16acec6dcbbc686092/search/result/shops",params
+                                ,new DisposeDataHandler(new DisposeDataListener() {
+                        @Override
+                        public void onSuccess(Object responseObj) {
+                            try {
+                                checkResult = (CheckResult<List<SearchShops>>) GsonUtil.fromJson(responseObj.toString(),
+                                        new TypeToken<CheckResult<List<SearchShops>>>(){}.getType());
+                                if (checkResult.isSuccess()){
+                                    onGetModel.getModel(checkResult.getData());
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                LogUtils.e("dd","00000000");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Object error) {
+
+                        }
+                    }));
+        } catch (NetworkErrorException e) {
+            e.printStackTrace();
+        }
+    }
+}
